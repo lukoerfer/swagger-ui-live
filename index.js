79daa74e6d1249b9860c27de3cdefe9ff2614b1d
Lukas Körfer <lu.koerfer@googlemail.com>
2019-06-22 04:05:08 +0200

    Initial version

---
+++ index.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const cheerio = require('cheerio');
+
+const swaggerUI = require('swagger-ui-dist').absolutePath();
+const swaggerLive = path.resolve(__dirname);
+
+function index() {
+	const $ = cheerio.load(fs.readFileSync(swaggerUI + "/index.html"));
+	$('script').last().remove();
+	$('body').append('<script src="./swagger-ui-live.js"> </script>');
+	$('body').append('<script src="./socket.io/socket.io.js"> </script>');
+	return $.html();
+}
+
+module.exports = function(spec, options = {}) {
+	options.port = options.port || 8000;
+	options.host = options.host || '127.0.0.1';
+
+	let app = express();
+	app.get('/', (req, res) => res.send(index()));
+	app.use(express.static(swaggerUI, { index: false }));
+	app.use(express.static(swaggerLive));
+	let server = http.createServer(app);
+	let io = socketio(server);
+
+	io.on('connection', socket => {
+		socket.on('ready', data => {
+			socket.emit('update', spec);
+		});
+	});
+	
+	server.listen(options.port, options.host);
+
+	return {
+		update: function(value) {
+			spec = value;
+			io.sockets.emit('update', spec);
+		},
+		stop: function() {
+			server.stop();
+		}
+	};
+};
---
+++ swagger-ui-live.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+    const ui = SwaggerUIBundle({
+        dom_id: '#swagger-ui',
+        deepLinking: true,
+        presets: [
+            SwaggerUIBundle.presets.apis,
+            SwaggerUIStandalonePreset
+        ],
+        plugins: [
+            SwaggerUIBundle.plugins.DownloadUrl
+        ],
+        layout: "StandaloneLayout"
+    });
+
+    var socket = io();
+    socket.on('update', data => {
+        ui.specActions.updateSpec(data);
+    });
+    socket.emit('ready', null);
+};
