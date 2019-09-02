742069cfbd3f154459e0fd0cbef9847d76f07e09
Lukas Körfer <lu.koerfer@googlemail.com>
2019-09-02 13:39:12 +0200

    Applied standard code style

--- dist/swagger-ui-live.js
+++ dist/swagger-ui-live.js
@@ -1,20 +1,22 @@
-window.onload = function() {
-    const ui = SwaggerUIBundle({
-        dom_id: '#swagger-ui',
-        deepLinking: true,
-        presets: [
-            SwaggerUIBundle.presets.apis,
-            SwaggerUIStandalonePreset
-        ],
-        plugins: [
-            SwaggerUIBundle.plugins.DownloadUrl
-        ],
-        layout: "StandaloneLayout"
-    });
+/* global io:false SwaggerUIBundle:false, SwaggerUIStandalonePreset:false */
 
-    var socket = io();
-    socket.on('update', data => {
-        ui.specActions.updateSpec(data);
-    });
-    socket.emit('ready', null);
-};
+window.onload = function () {
+  const ui = SwaggerUIBundle({
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: 'StandaloneLayout'
+  })
+
+  var socket = io()
+  socket.on('update', data => {
+    ui.specActions.updateSpec(data)
+  })
+  socket.emit('ready', null)
+}
--- index.js
+++ index.js
@@ -1,47 +1,47 @@
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
-const http = require('http');
-const socketio = require('socket.io');
-const cheerio = require('cheerio');
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+const http = require('http')
+const socketio = require('socket.io')
+const cheerio = require('cheerio')
 
-const swaggerUI = require('swagger-ui-dist').absolutePath();
-const swaggerLive = path.join(path.resolve(__dirname), 'dist');
+const swaggerUI = require('swagger-ui-dist').absolutePath()
+const swaggerLive = path.join(path.resolve(__dirname), 'dist')
 
-function index() {
-	const $ = cheerio.load(fs.readFileSync(swaggerUI + "/index.html"));
-	$('script').last().remove();
-	$('body').append('<script src="./swagger-ui-live.js"> </script>');
-	$('body').append('<script src="./socket.io/socket.io.js"> </script>');
-	return $.html();
+function index () {
+  const $ = cheerio.load(fs.readFileSync(swaggerUI + '/index.html'))
+  $('script').last().remove()
+  $('body').append('<script src="./socket.io/socket.io.js"> </script>')
+  $('body').append('<script src="./swagger-ui-live.js"> </script>')
+  return $.html()
 }
 
-module.exports = function(spec, options = {}) {
-	options.port = options.port || 3000;
-	options.host = options.host || '127.0.0.1';
+module.exports = function (spec, options = {}) {
+  options.port = options.port || 3000
+  options.host = options.host || '127.0.0.1'
 
-	let app = express();
-	app.get('/', (req, res) => res.send(index()));
-	app.use(express.static(swaggerUI, { index: false }));
-	app.use(express.static(swaggerLive));
-	let server = http.createServer(app);
-	let io = socketio(server);
+  const app = express()
+  app.get('/', (req, res) => res.send(index()))
+  app.use(express.static(swaggerUI, { index: false }))
+  app.use(express.static(swaggerLive))
+  const server = http.createServer(app)
+  const io = socketio(server)
 
-	io.on('connection', socket => {
-		socket.on('ready', data => {
-			socket.emit('update', spec);
-		});
-	});
-	
-	server.listen(options.port, options.host);
+  io.on('connection', socket => {
+    socket.on('ready', data => {
+      socket.emit('update', spec)
+    })
+  })
 
-	return {
-		update: function(value) {
-			spec = value;
-			io.sockets.emit('update', spec);
-		},
-		stop: function() {
-			server.stop();
-		}
-	};
-};
+  server.listen(options.port, options.host)
+
+  return {
+    update: function (value) {
+      spec = value
+      io.sockets.emit('update', spec)
+    },
+    stop: function () {
+      server.stop()
+    }
+  }
+}
